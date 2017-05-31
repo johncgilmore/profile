3c5653877b022a500fb2a101777f847fe13ee03b
johncgilmore <johncgilmore@gmail.com>
2017-05-31 13:42:03 -0700

    response.write some html to the client from the server listening to 8888

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-var html = require("html");
-var fs = require("fs");
-  
+var http = require("http");
+var onRequest = function(request, response){
+  console.log(request.method + "request received at /");
+  pageData = {}
+  pageData.headline = "Hello World"
+  pageData.listTitle = "Next Steps"
+  response.writeHead(200, {"Content-Type": "text/html"})
+  response.write("<html><head><script src='jquery-3.2.1.min.js'></script></head>")
+  response.write("<body><h1>" + pageData.headline + "</h1><h2>" + pageData.listTitle + "</h2>");
+  response.write("<ul>");
+  response.write("<li>send file instead of html text</li><li>get dependencies into package.json</li>");
+  response.write("<form action='/next' method='POST' style='background-color:#99ffff'>");
+  response.write("<input id='next-step' type='text' name='next-step'</ >");
+  response.write("<input type='submit' name='submit' value='add' style='background-color:#ffff99'</ ></form>");
+  response.write("</ul>");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
+};
 
-html.createServer(onRequest).listen(8888);
+
+var PORT = 8888
+http.createServer(onRequest).listen(PORT);
+console.log("listening at port " + PORT);
